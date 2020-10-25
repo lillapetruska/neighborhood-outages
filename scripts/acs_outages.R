293a0330798ceef9772d2b4079d6048e68714bbc
Lilla Petruska <petruskalilla@gmail.com>
2020-10-24 21:48:05 -0700

    add num outages

--- scripts/acs_outages.R
+++ scripts/acs_outages.R
@@ -22,14 +22,15 @@
 # Update log:
 # 10/22/20 - created summary stats for PG&E outaged data by census tract and
 # joined this data onto ACS census tract data.
-# 10/22/20 - fixed binary flag, converted NA's in outage stats after ACS merge 
+# 10/22/20 - fixed binary flag, converted NA's in outage stats after ACS merge
 # to 0s
 
 # Setup -------------------------------------------------------------------
 # Packages:
 library(tidyverse)
+library(here)
 
-# Directories: 
+# Directories:
 homedir <- "E:/neighborhood-outages/"
 workdir <- "cleaned_data/"
 savedir <- "cleaned_data/"
@@ -38,6 +39,10 @@ setwd(homedir)
 # Parameters
 outages_filepath <- paste0(homedir, workdir, "outages_clean.csv")
 acs_filepath <- paste0(homedir, workdir, "acs_clean.csv")
+
+outages_filepath <- here("cleaned_data/outages_clean.csv")
+acs_filepath <- here("cleaned_data/acs_clean.csv")
+
 
 # Import data:
 outages <- read_csv(outages_filepath)
@@ -50,31 +55,33 @@ outages_grouped <-
   group_by(GEOID) %>%
   summarise(
     median_outage_duration_hr = median(outage_duration_hr),
-    median_mean_cust_affected = median(mean_cust_affected)
-  ) %>% 
-  ungroup() %>% 
+    median_mean_cust_affected = median(mean_cust_affected),
+    num_outages = n()
+  ) %>%
+  ungroup() %>%
   mutate(
-    above_median_cust_affected = 
+    above_median_cust_affected =
       if_else(
         median_mean_cust_affected > median(median_mean_cust_affected), 1, 0
       )
-  ) %>% 
+  ) %>%
   # remove median of mean customers affected column
   select(-median_mean_cust_affected)
 
 acs_outages <-
   acs %>%
-  left_join(outages_grouped, by = "GEOID") %>% 
+  left_join(outages_grouped, by = "GEOID") %>%
   mutate(
     median_outage_duration_hr = replace_na(median_outage_duration_hr, 0),
-    above_median_cust_affected = replace_na(above_median_cust_affected, 0)
+    above_median_cust_affected = replace_na(above_median_cust_affected, 0),
+    num_outages = replace_na(num_outages, 0)
   )
 
 # CA census tracts that we don't have outage data for, as they are likely
 # not serviced by PG&E.
-non_outage_tracts <- 
-  setdiff(acs$GEOID, outages_grouped$GEOID) %>% 
-  as_tibble() %>% 
+non_outage_tracts <-
+  setdiff(acs$GEOID, outages_grouped$GEOID) %>%
+  as_tibble() %>%
   rename(non_pge_tract = value)
 
 rm(acs, outages, outages_grouped)
